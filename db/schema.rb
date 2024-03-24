e1841fd7df2b93f6a1fcda4f973203cf5aedba01
Rose Brennan <rose@roses-mbp-2.lan>
2024-03-24 15:27:02 -0600

    Create Drivers

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_24_211922) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_24_212426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "drivers", force: :cascade do |t|
+    t.string "home_address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rides", force: :cascade do |t|
     t.string "start_address", null: false
