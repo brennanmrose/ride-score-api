56df002a537cb80fbe2b4d2902d4e82122b591fb
Rose Brennan <rose@roses-mbp-2.lan>
2024-03-25 12:24:15 -0600

    Add without_driver scope to Ride

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,4 +4,6 @@ class Ride < ApplicationRecord
   belongs_to :driver, optional: true
 
   validates :start_address, :destination_address, presence: true
+
+  scope :without_driver, -> { where(driver_id: nil) }
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe Ride, type: :model do
     it { is_expected.to validate_presence_of(:start_address) }
     it { is_expected.to validate_presence_of(:destination_address) }
   end
+
+  describe '.without_driver' do
+    let!(:rides_without_driver) { create_list(:ride, 2) }
+    let!(:ride_with_driver) { create(:ride, :with_driver) }
+
+    it 'returns rides without a driver assigned' do
+      expect(described_class.without_driver).to eq(rides_without_driver)
+    end
+
+    it 'does not return rides with a driver assigned' do
+      expect(described_class.without_driver).not_to include(ride_with_driver)
+    end
+  end
 end
